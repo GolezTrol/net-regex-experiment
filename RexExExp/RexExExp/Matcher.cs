ec3baf732d59fcb98b66b750c94a7451d470cec1
Jos Visser <j.visser@coolblue.nl>
2020-04-22 22:28:48 +0200

    Add Test setup and cheat matcher using existing regex library

---
+++ RexExExp/RexExExp/Matcher.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RexExExp
+{
+    public class Matcher
+    {
+        public static bool IsMatch(string input, string pattern)
+        {
+            return Regex.IsMatch(input, $"^{pattern}$");
+        }
+    }
+}
--- RexExExp/RexExExp/Program.cs
+++ RexExExp/RexExExp/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Security;
+using System.Text.RegularExpressions;
 
 namespace RexExExp
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Verify(string input, string pattern, bool expected, string reason)
         {
-            Console.WriteLine("Hello World!");
+            bool ok = Matcher.IsMatch(input, pattern) == expected;
+            Console.WriteLine($"{input}\n{pattern}\n{reason}\n" + (ok ? "ok" : "FAILED"));
+
+            return ok ? 0 : 1; // Return 1 in case of error
+        }
+
+        static int Main(string[] args)
+        {
+            var errors = 
+                Verify("aa", "a", false, "'a' does not match the entire string 'aa'.") +
+                Verify("aa", "a*", true, "'*' means zero or more of the preceding element, 'a'.Therefore, by repeating 'a' once, it becomes 'aa'.") +
+                Verify("ab", ".*", true, "'.' means 'zero or more () of any character (.)'.") +
+                Verify("aab", "c*a*b", true, "c can be repeated 0 times, a can be repeated 2 times. Therefore, it matches 'aab'.") +
+                Verify("mississippi", "mis*is*p*.", false, "Everything matches, except the 'i' beween 'ss' and 'pp'.") +
+                0;
+
+            Console.WriteLine($"\n---------------\n{errors} errors");
+
+            return errors;
         }
     }
 }
